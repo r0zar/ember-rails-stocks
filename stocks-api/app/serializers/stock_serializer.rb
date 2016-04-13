c7307d1d46a4477b41276eed0173146818a622e9
Ross Ragsdale <ross.ragsdale@gmail.com>
2016-04-13 13:40:47 -0500

    Update stock_serializer.rb

--- stocks-api/app/serializers/stock_serializer.rb
+++ stocks-api/app/serializers/stock_serializer.rb
@@ -1,3 +1,3 @@
 class StockSerializer < ActiveModel::Serializer
-  attributes :id, :symbol, :name, :last_sale, :market_cap
+  attributes :id, :symbol, :name, :last_sale, :market_cap, :adr_tso, :ipo_year, :sector, :industry, :summary_quote
 end
