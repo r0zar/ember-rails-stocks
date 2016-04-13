f4403ef59fe12263e8e750b2b19adb9d361755cd
Ross Ragsdale <ross.ragsdale@gmail.com>
2016-04-13 19:06:32 +0000

    Add stocks templates.

---
+++ stocks/tmp/babel-output_path-3ALypoBh.tmp/stocks/templates/stocks/index.js
@@ -0,0 +1,39 @@
+define("stocks/templates/stocks/index", ["exports"], function (exports) {
+  exports["default"] = Ember.HTMLBars.template((function () {
+    return {
+      meta: {
+        "fragmentReason": {
+          "name": "missing-wrapper",
+          "problems": ["empty-body"]
+        },
+        "revision": "Ember@2.4.4",
+        "loc": {
+          "source": null,
+          "start": {
+            "line": 1,
+            "column": 0
+          },
+          "end": {
+            "line": 1,
+            "column": 0
+          }
+        },
+        "moduleName": "stocks/templates/stocks/index.hbs"
+      },
+      isEmpty: true,
+      arity: 0,
+      cachedFragment: null,
+      hasRendered: false,
+      buildFragment: function buildFragment(dom) {
+        var el0 = dom.createDocumentFragment();
+        return el0;
+      },
+      buildRenderNodes: function buildRenderNodes() {
+        return [];
+      },
+      statements: [],
+      locals: [],
+      templates: []
+    };
+  })());
+});
