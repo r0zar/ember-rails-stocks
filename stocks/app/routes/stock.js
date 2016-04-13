c68f5c7fbfad3c55cbe9e2534075c5262d38fe8f
Ross Ragsdale <ross.ragsdale@gmail.com>
2016-04-13 21:01:39 +0000

    Minor cleanup.

--- stocks/app/routes/stock.js
+++
@@ -1,8 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Route.extend({
-  model(param) {
-    Ember.Logger.debug('json: ', this.store.query('stock', param));
-    return this.store.query('stock', param);
-  }
-});
